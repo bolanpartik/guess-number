f2046c1c76c735c29897e96c639980d270c1b05b
Partik Bolan <bolanpartik@gmail.com>
2024-08-31 23:10:21 +0530

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <img src='' alt="logo" />
+                <p>Guess Number</p>
+            </div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Contact</a>
+                <a href="#">About</a>
+            </div>
+        </header>
+    )
+}

15a115ad6b52bc1a0bfc8b1d8d731d7f31800721
Partik Bolan <bolanpartik@gmail.com>
2024-08-31 23:51:15 +0530

    add header component styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import Logo from '../assets/logo.png'
 
 export default function Header(){
     return(
-        <header>
-            <div>
-                <img src={Logo} alt="logo" />
-                <p>Guess Number</p>
+        <header className='flex justify-between h-16 shadow-sm shadow-white w-full text-white backdrop-blur-sm bg-black/50'>
+            <div className='flex items-center h-full ml-5'>
+                <img src={Logo} alt="log" className='h-14'/>
+                <p className='ml-3 text-2xl'>Guess Number</p>
             </div>
-            <div>
-                <a href="#">Home</a>
-                <a href="#">Contact</a>
-                <a href="#">About</a>
+            <div className='flex items-center gap-5 mr-5'>
+                <a href="#" className='transition-all hover:scale-110'>Home</a>
+                <a href="#" className='transition-all hover:scale-110'>Contact</a>
+                <a href="#" className='transition-all hover:scale-110'>About</a>
             </div>
         </header>
     )

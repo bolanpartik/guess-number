4fcd94d6d76ee7fab85b6566fbbb137e63dbe713
Partik Bolan <bolanpartik@gmail.com>
2024-09-01 22:40:19 +0530

    add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Page from './components/Page'
 function App() {
 
   return (
-    <div>
+    <div className='bg-backgroundImg h-screen bg-cover bg-no-repeat flex flex-col items-center' >
       <Header/>
       <Page/>
     </div>

a615cf4052c6736ae8625864745446c7ffcf727c
Partik Bolan <bolanpartik@gmail.com>
2024-09-02 13:32:17 +0530

    update the state when input change

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,15 @@
 export default function Input({ userNum,setUserNum }) {
+
+    const handleInputChange = (e) => {
+        setUserNum(e.target.value)
+    }
     
     return (
         <input
             type="number"
             placeholder="Enter a number between 1 to 100"
             value={userNum}
+            onChange={handleInputChange}
         />
     )
 }

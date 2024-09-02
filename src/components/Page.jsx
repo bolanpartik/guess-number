d8a66189f7ff0006c97ad61653a5b8b3d974a0d6
Partik Bolan <bolanpartik@gmail.com>
2024-09-02 13:36:36 +0530

    import button component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import Button from "./Button";
 import Input from "./Input";
 
 export default function Page() {
     const [userNum, setUserNum] = useState('')
-    
+
     return (
         <div>
             <p>Guess Number</p>
             <Input userNum={userNum} setUserNum={setUserNum} />
+            <div>
+                <Button />
+                <Button />
+            </div>
         </div>
     )
 }

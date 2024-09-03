341908ce4f4613407eefae87b969cc8eaef6a2d9
Partik Bolan <bolanpartik@gmail.com>
2024-09-03 09:04:42 +0530

    add styles for button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
-export default function Button({title,handleClick}){
+export default function Button({ title, handleClick }) {
 
-    return(
-        <button onClick={handleClick}>{title}</button>
+    return (
+        <button
+            className="rounded-lg mt-7 p-2 px-4 hover:bg-[#F2B316] text-white hover:text-[#1D1B16] shadow-inner shadow-blue-400 hover:shadow-lg"
+            onClick={handleClick}>
+            {title}
+        </button>
     )
 }
